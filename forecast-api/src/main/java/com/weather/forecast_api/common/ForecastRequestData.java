558ab562a5818647f714e1fb4ba6a3862a4cf1bc
guycafif <g.cafif@gmail.com>
2024-05-31 17:33:54 +0300

    Built service request from input params

--- forecast-api/src/main/java/com/weather/forecast_api/common/ForecastRequestData.java
+++ forecast-api/src/main/java/com/weather/forecast_api/common/ForecastRequestData.java
@@ -1,4 +1,16 @@
 package com.weather.forecast_api.common;
 
+
+import com.weather.forecast_api.common.utils.ForecastCondition;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ForecastRequestData {
+
+    private ForecastCondition condition;
+    private double latitude;
+    private double longitude;
+
 }
--- forecast-api/src/main/java/com/weather/forecast_api/controllers/ForecastController.java
+++ forecast-api/src/main/java/com/weather/forecast_api/controllers/ForecastController.java
@@ -1,5 +1,7 @@
 package com.weather.forecast_api.controllers;
 
+import com.weather.forecast_api.common.ForecastRequestData;
+import com.weather.forecast_api.common.utils.ForecastCondition;
 import com.weather.forecast_api.services.ForecastService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +16,31 @@ public class ForecastController {
     @PostMapping("/insight?condition={condition}&lat={lat}&lon={lon}")
     public Object handleForecastInsightsRequest(@PathVariable String condition, @PathVariable String lat, @PathVariable String lon){
 
+        ForecastRequestData forecastRequestData = createRequestData(condition,lat, lon);
 
         return null;
+    }
+
+    private ForecastRequestData createRequestData(String condition,String lat, String lon) {
+        ForecastRequestData forecastRequestData = new ForecastRequestData();
+
+        ForecastCondition forecastCondition = ForecastCondition.getFromConditionName(condition);
+        if(forecastCondition != null ){
+            forecastRequestData.setCondition(forecastCondition);
+        }
+        else{
+            throw new IllegalArgumentException(String.format("Condition %s does not exist", condition));
+        }
+
+        try
+        {
+            forecastRequestData.setLatitude( Double.parseDouble(lat));
+            forecastRequestData.setLongitude(Double.parseDouble(lon));
+        }
+        catch (NumberFormatException | NullPointerException e){
+            throw new IllegalArgumentException(String.format(String.format("Invalid latitude: %s or longitude %s values, must be a numeric value", lat, lon)));
+        }
+
+        return forecastRequestData;
     }
 }
